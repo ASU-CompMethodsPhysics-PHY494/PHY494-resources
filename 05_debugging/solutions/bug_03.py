842167f888f0e170b2870eb55d1d246648491a91
Oliver Beckstein <orbeckst@gmail.com>
2021-02-24 15:00:37 -0700

    debugging exercise solutions

---
+++ 05_debugging/solutions/bug_03.py
@@ -0,0 +1,8 @@
+# bug 3
+# https://asu-compmethodsphysics-phy494.github.io/ASU-PHY494/2019/02/05/05_Debugging/#activity-fix-as-many-bugs-as-possible
+
+# Print "error" for input 0:
+x = float(input("Enter non-zero number --> "))
+if x == 0:
+   print("ERROR: number cannot be 0")
+
